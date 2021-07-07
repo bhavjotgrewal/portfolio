fef77a1b366ae51688d66394ab46d8703de982ac
Bhavjot Grewal <bhavjot.grewal@mail.utoronto.ca>
2021-07-06 23:16:19 -0400

    Site

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        my personal <br /> 
+        portfolio.
+      </SectionTitle>
+      <SectionText>
+      I'm in second year studying Computer Science and Statistics at the University of Toronto. This is my portfolio. It's where I showcase my projects and more.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Stock Alert Bot',
+    description: "Developed an automatic bot using Python and BeautifulSoup to check if products are in stock",
+      image: '/images/1.png',
+      tags: ['Python', 'BeautifulSoup', 'Rich', 'pyttsx3'],
+    source: 'https://github.com/bhavjotgrewal/in-stock-alert',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Video Chat Web App',
+    description:"Created a full stack mobile-friendly web app to make video calls",
+    image: '/images/2.png',
+    tags: ['React', 'WebRTC', 'Socket.IO'],
+    source: 'https://github.com/bhavjotgrewal/video-chat-revamped',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'Lyrics for Spotify',
+    description: "Designed a chrome extension to deliver lyrics for listeners on any version of the Spotify app. Simply login, and view the current lyrics!",
+      image: '/images/3.png',
+      tags: ['Spotify API', 'oAuth 2.0', 'Lyrics.ovh API'],
+    source: 'https://github.com/bhavjotgrewal/lyric-finder',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Crypto Price Tracker',
+    description: "Built a web app using React to track the price of cryptocurrencies",
+    image: '/images/crypto.png',
+    tags: ['CoinGecko API', 'React', 'axios', 'Netlify'],
+    source: 'https://github.com/bhavjotgrewal/crypto-price-web-app',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
